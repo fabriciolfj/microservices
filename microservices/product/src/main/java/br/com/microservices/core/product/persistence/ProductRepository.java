ac7a89eb3adf0ad4ee80a9eb74b2c77888c75606
fabricio <fabricio.jacob@outlook.com>
2020-03-31 09:31:03 -0300

    add

---
+++ microservices/product/src/main/java/br/com/microservices/core/product/persistence/ProductRepository.java
@@ -0,0 +1,10 @@
+package br.com.microservices.core.product.persistence;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends PagingAndSortingRepository<ProductEntity, String> {
+
+    Optional<ProductEntity> findByProductId(int productId);
+}
---
+++ microservices/recommendation/src/main/java/br/com/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -0,0 +1,10 @@
+package br.com.microservices.core.recommendation.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+
+    List<RecommendationEntity> findByProductId(int productId);
+}
