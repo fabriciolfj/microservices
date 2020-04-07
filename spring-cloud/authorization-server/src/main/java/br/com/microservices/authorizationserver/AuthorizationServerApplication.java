af9c24bdfd47d76ae8cd58101a34c395d13e2610
fabricio <fabricio.jacob@outlook.com>
2020-04-07 14:55:10 -0300

    add server configuration.

---
+++ spring-cloud/authorization-server/src/main/java/br/com/microservices/authorizationserver/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
