45c8a4abe505ff5e7bf44af163a87916807d2b83
fabricio <fabricio.jacob@outlook.com>
2020-04-07 21:05:47 -0300

    add

--- spring-cloud/authorization-server/src/main/java/br/com/microservices/authorizationserver/AuthorizationServerConfiguration.java
+++ spring-cloud/authorization-server/src/main/java/br/com/microservices/authorizationserver/AuthorizationServerConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
@@ -51,11 +52,11 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     boolean jwtEnabled;
 
     public AuthorizationServerConfiguration(
-            AuthenticationManager authenticationManager,
+            AuthenticationConfiguration authenticationConfiguration,
             KeyPair keyPair,
             @Value("${security.oauth2.authorizationserver.jwt.enabled:true}") boolean jwtEnabled) throws Exception {
 
-        this.authenticationManager = authenticationManager;
+        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
         this.keyPair = keyPair;
         this.jwtEnabled = jwtEnabled;
     }
@@ -131,7 +132,7 @@ class UserConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/actuator/**").permitAll()
-                .mvcMatchers("/.well-know/jwks.json").permitAll()
+                .mvcMatchers("/.well-known/jwks.json").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
--- spring-cloud/authorization-server/src/test/java/br/com/microservices/authorizationserver/AuthorizationServerApplicationTests.java
+++ spring-cloud/authorization-server/src/test/java/br/com/microservices/authorizationserver/AuthorizationServerApplicationTests.java
@@ -2,15 +2,41 @@ package br.com.microservices.authorizationserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = {"eureka.client.enabled=false"})
+@AutoConfigureMockMvc
 public class AuthorizationServerApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void requestTokenWhenUsingPasswordGrandTypeThenOk() throws Exception {
+		this.mvc.perform(post("/oauth/token")
+				.param("grant_type", "password")
+				.param("username", "fabricio")
+				.param("password", "password")
+				.header("Authorization", "Basic cmVhZGVyOnNlY3JldA=="))
+				.andExpect(status().isOk());
 	}
+
+	@Test
+	public void requestJwkSetWhenUsingDefaultsThenOk()
+			throws Exception {
+
+		this.mvc.perform(get("/.well-known/jwks.json"))
+				.andExpect(status().isOk());
+	}
+
 
 }
