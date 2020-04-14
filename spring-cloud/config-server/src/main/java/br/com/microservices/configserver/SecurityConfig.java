cd691cdb3024aa1c5c9a9a9ca2ac667c660721e7
fabricio <fabricio.jacob@outlook.com>
2020-04-14 18:48:46 -0300

    Finalizada migração para kubernetes.

--- spring-cloud/config-server/src/main/java/br/com/microservices/configserver/SecurityConfig.java
+++ spring-cloud/config-server/src/main/java/br/com/microservices/configserver/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                     .disable()
                 .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                     .httpBasic();
